d8084f7e2f47246de335186b031edccdb9dbcc41
Raja <31615345+PushparajaMurugan@users.noreply.github.com>
2023-12-15 01:32:00 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+import numpy as np
+import pickle
+
+
+def SimpleCalculator(args):
+
+    total = args.number1 + args.number2
+    with open(f"{args.save_dir}/total.pickle", "wb") as f:
+        pickle.dump(total, f)
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Download.')
+    parser.add_argument('--save_dir', default='data', type=str, help='data directory')
+    parser.add_argument('--number1', default=20, type=int, help='data directory')
+    parser.add_argument('--number2', default=50, type=int, help='data directory')
+
+    args = parser.parse_args()
+    SimpleCalculator(args)
